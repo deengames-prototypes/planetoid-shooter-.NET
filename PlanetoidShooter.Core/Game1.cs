96f0cf75b9e9aaa0fb3471ad9246237e8528b166
ashes999 <ashes999@yahoo.com>
2015-10-08 14:42:41 -0400

    Recreated/broke-out core project; added screens and sprites.

--- PlanetoidShooter.Core/Game1.cs
+++ PlanetoidShooter.Core/Game1.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
+using Nucleus;
 
 #endregion
 
@@ -13,81 +14,13 @@ namespace PlanetoidShooter.Core
 	/// <summary>
 	/// This is the main type for your game.
 	/// </summary>
-	public class Game1 : Game
+	public class Game1 : CommonGame
 	{
-		GraphicsDeviceManager graphics;
-		SpriteBatch spriteBatch;
-		Texture2D sprite;
-
-		public Game1 ()
-		{
-			graphics = new GraphicsDeviceManager (this);
-			Content.RootDirectory = "Content";	            
-			graphics.IsFullScreen = false;		
-			graphics.PreferredBackBufferWidth = 960;
-			graphics.PreferredBackBufferHeight = 540;
-		}
-
-		/// <summary>
-		/// Allows the game to perform any initialization it needs to before starting to run.
-		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
-		/// and initialize them as well.
-		/// </summary>
-		protected override void Initialize ()
-		{
-			// TODO: Add your initialization logic here
-			base.Initialize ();
-				
-		}
-
-		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
-		/// </summary>
-		protected override void LoadContent ()
-		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			spriteBatch = new SpriteBatch (GraphicsDevice);
-
-			//TODO: use this.Content to load your game content here
-			sprite = Content.Load<Texture2D>("Icon.png");
-		}
-
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Update (GameTime gameTime)
-		{
-			// For Mobile devices, this logic will close the Game when the Back button is pressed
-			// Exit() is obsolete on iOS
-			#if !__IOS__
-			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-			    Keyboard.GetState ().IsKeyDown (Keys.Escape)) {
-				Exit ();
-			}
-			#endif
-			// TODO: Add your update logic here			
-			base.Update (gameTime);
-		}
-
-		/// <summary>
-		/// This is called when the game should draw itself.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Draw (GameTime gameTime)
-		{
-			graphics.GraphicsDevice.Clear (Color.Black);
-		
-			//TODO: Add your drawing code here
-			spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.NonPremultiplied);
-			spriteBatch.Draw (sprite, Vector2.Zero);
-			spriteBatch.End ();
-            
-			base.Draw (gameTime);
-		}
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Screen.ShowScreen(new CoreGameScreen());
+        }
 	}
 }
 
---
+++ PlanetoidShooter.Core/Screens/CoreGameScreen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nucleus;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PlanetoidShooter.Core
+{
+    public class CoreGameScreen : Screen
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            this.Add(new SpriteComponent("Icon"));
+        }
+    }
+}
+
