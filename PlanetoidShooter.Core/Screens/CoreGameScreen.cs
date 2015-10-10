6afd07655f208328f1e201513691efaac201ff50
ashes999 <ashes999@yahoo.com>
2015-10-10 08:29:43 -0400

    Image rotates; tried to add fonts (TTF importing is broken in MonoGame 3.4)

--- PlanetoidShooter.Core/Screens/CoreGameScreen.cs
+++ PlanetoidShooter.Core/Screens/CoreGameScreen.cs
@@ -9,13 +9,20 @@ namespace PlanetoidShooter.Core
 {
     public class CoreGameScreen : Screen
     {
+        private Entity e;
+
         public override void Initialize()
         {
             base.Initialize();
-            var e = new Entity();
+            e = new Entity();
             e.Add(new ImageComponent("Icon"));
-            e.Add(new TwoDComponent(100, 50));
+            e.Add(new TwoDComponent(50, 50));
             this.Add(e);
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            e.Get<TwoDComponent>().Rotation += 1;
         }
     }
 }
