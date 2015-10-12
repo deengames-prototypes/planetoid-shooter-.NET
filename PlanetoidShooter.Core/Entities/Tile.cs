5399a929f22e99409710e1a508b0621119935545
ashes999 <ashes999@yahoo.com>
2015-10-11 23:40:17 -0400

    Added tile-drawing. FPS is abysmal.

---
+++ PlanetoidShooter.Core/Entities/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nucleus;
+using Nucleus.Ecs.Components;
+using Nucleus.Ecs;
+
+namespace PlanetoidShooter.Core.Entities
+{
+    public class Tile : Entity
+    {
+        public Tile(int x, int y, int tileWidth, int tileHeight, bool isWall)
+        {
+            var s = new SpriteSheetComponent("Tiles", tileWidth, tileHeight);
+            if (!isWall)
+            {
+                s.CurrentFrame = 1;
+            }
+            this.Add(new TwoDComponent(x * tileWidth, y * tileHeight));
+            this.Add(s);
+        }
+    }
+}
+
--- PlanetoidShooter.Core/Screens/CoreGameScreen.cs
+++ PlanetoidShooter.Core/Screens/CoreGameScreen.cs
@@ -4,25 +4,30 @@ using Microsoft.Xna.Framework.Graphics;
 using Nucleus.Core;
 using Nucleus.Ecs.Components;
 using Nucleus.Ecs;
+using PlanetoidShooter.Core.Entities;
 
 namespace PlanetoidShooter.Core
 {
     public class CoreGameScreen : Screen
     {
-        private Entity e;
+        private const int TileWidth = 32;
+        private const int TileHeight = 32;
 
         public override void Initialize()
         {
             base.Initialize();
-            e = new Entity();
-            e.Add(new ImageComponent("Icon"));
-            e.Add(new TwoDComponent(50, 50));
-            this.Add(e);
-        }
+            int widthInTiles = (int)Math.Ceiling(1d * this.Width / TileWidth);
+            int heightInTiles = (int)Math.Ceiling(1d * this.Height / TileHeight);
+            int count = 0;
 
-        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
-        {
-            e.Get<TwoDComponent>().Rotation += 1;
+            for (int y = 0; y < heightInTiles; y++)
+            {
+                for (int x = 0; x < widthInTiles; x++)
+                {
+                    bool isWall = (x == 0 || y == 0 || x == widthInTiles - 1 || y == heightInTiles - 1);
+                    this.Add(new Tile(x, y, TileWidth, TileHeight, isWall));
+                }
+            }
         }
     }
 }
